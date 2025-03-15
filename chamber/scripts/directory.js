61e7ba5f5888b7ad63e4ea0ca98dbca09c9de8e1
Anna Abbott <aabbott@byupathway.edu>
2025-03-15 15:16:59 -0500

    added grid view/ list view capability

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,10 +1,15 @@
 const path = "scripts/members.json";
 const cards = document.querySelector("#cards");
+const gridViewBtn = document.querySelector("#gridDisplay");
+const listViewBtn = document.querySelector("#listDisplay");
 
 const displayMembers = (members) => {
+  cards.innerHTML = "";
   members.forEach((member) => {
     let card = document.createElement("section");
     cards.appendChild(card);
+    cards.classList.add("grid");
+    cards.classList.remove("list");
     let companyName = document.createElement("h2");
     companyName.textContent = `${member.name}`;
     card.appendChild(companyName);
@@ -28,6 +33,29 @@ const displayMembers = (members) => {
   });
 };
 
+const displayMembersList = (members) => {
+  cards.innerHTML = "";
+  members.forEach((member) => {
+    let row = document.createElement("section");
+    cards.appendChild(row);
+    cards.classList.add("list");
+    cards.classList.remove("grid");
+    let companyName = document.createElement("h2");
+    companyName.textContent = `${member.name}`;
+    row.appendChild(companyName);
+    let address = document.createElement("p");
+    address.textContent = `${member.address}`;
+    row.appendChild(address);
+    let phone = document.createElement("p");
+    phone.textContent = `${member.phone}`;
+    row.appendChild(phone);
+    let website = document.createElement("a");
+    website.textContent = `${member.website}`;
+    row.appendChild(website);
+    website.setAttribute("href", `${member.website}`);
+  });
+};
+
 async function getMemberData() {
   try {
     const response = await fetch(path);
@@ -35,7 +63,12 @@ async function getMemberData() {
       throw new Error(`Response status: ${response.status}`);
     }
     const data = await response.json();
-    displayMembers(data);
+    gridViewBtn.addEventListener("click", () => {
+      displayMembers(data);
+    });
+    listViewBtn.addEventListener("click", () => {
+      displayMembersList(data);
+    });
   } catch (error) {
     console.error(error.message);
   }
