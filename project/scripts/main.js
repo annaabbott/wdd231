fbb0119ee3166b79942925d3472b3cc7fe5fe888
Anna Abbott <aabbott@byupathway.edu>
2025-04-11 12:48:42 -0500

    completed thankyou page; added info to news cards

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -2,27 +2,44 @@ import {hamburgerBtn, navMenu} from "./navigation.mjs";
 import {weatherFetch} from "./weather.mjs";
 import {newsFetch} from "./news.mjs";
 import { createModal1, createModal0 } from "./eventsModal.mjs";
+import { displayFormData} from "./thankyou.mjs";
 
 hamburgerBtn.addEventListener("click", () => {
     navMenu.classList.toggle("open");
     // hamburgerBtn.classList.toggle("open");
   });
   
-  weatherFetch();
+ if (document.getElementById("newsContainer")) {
+   weatherFetch();
+ }
+ if (document.getElementById("weatherContainer")) {
   newsFetch();
+ }
+
 
   const btn00 = document.querySelector("#event00");
+  const btn01 = document.querySelector("#event01");
+
+if (btn00){
   btn00.addEventListener("click", () => {
     createModal0();
   });
+}
 
-  const btn01 = document.querySelector("#event01");
-  btn01.addEventListener("click", () => {
-    createModal1();
-  });
-
-
-  closeModal.addEventListener("click", () => {
-    modal.close();
-  });
+  if (btn01) {
+    btn01.addEventListener("click", () => {
+      createModal1();
+    });
   
+  }
+
+  const closeModal = document.querySelector("#closeModal");
+  if (closeModal) {
+    closeModal.addEventListener("click", () => {
+      modal.close();
+    });
+  
+  }
+  if (document.getElementById("formResults")) {
+    displayFormData()
+  }
