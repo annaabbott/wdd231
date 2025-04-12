0817b65c6204eff314b6498daaba8f1cdf956519
Anna Abbott <aabbott@byupathway.edu>
2025-04-12 15:49:19 -0500

    added Santa Cruz to weaher section

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -1,52 +1,73 @@
-import {hamburgerBtn, navMenu} from "./navigation.mjs";
-import {weatherFetch} from "./weather.mjs";
-import {newsFetch} from "./news.mjs";
+import { hamburgerBtn, navMenu } from "./navigation.mjs";
+import { weatherFetch } from "./weather.mjs";
+import { newsFetch } from "./news.mjs";
 import { createModal1, createModal0 } from "./eventsModal.mjs";
-import { displayFormData} from "./thankyou.mjs";
+import { displayFormData } from "./thankyou.mjs";
 
 hamburgerBtn.addEventListener("click", () => {
-    navMenu.classList.toggle("open");
-    // hamburgerBtn.classList.toggle("open");
-  });
-  
- if (document.getElementById("newsContainer")) {
-   weatherFetch();
- }
- if (document.getElementById("weatherContainer")) {
+  navMenu.classList.toggle("open");
+  // hamburgerBtn.classList.toggle("open");
+});
+
+if (document.getElementById("newsContainer")) {
+  weatherFetch();
+}
+if (document.getElementById("weatherContainer")) {
   newsFetch();
- }
+}
 
+const btn00 = document.querySelector("#event00");
+const btn01 = document.querySelector("#event01");
 
-  const btn00 = document.querySelector("#event00");
-  const btn01 = document.querySelector("#event01");
-
-if (btn00){
+if (btn00) {
   btn00.addEventListener("click", () => {
     createModal0();
   });
 }
 
-  if (btn01) {
-    btn01.addEventListener("click", () => {
-      createModal1();
-    });
-  
-  }
-
-  const closeModal = document.querySelector("#closeModal");
-  if (closeModal) {
-    closeModal.addEventListener("click", () => {
-      modal.close();
-    });
-  
-  }
-  if (document.getElementById("formResults")) {
-    displayFormData()
-  }
-
-  const visitCount = document.querySelector(".myVisitCount");
-let numVisits = Number(window.localStorage.getItem("myVisitCount")) || 0;
-console.log(numVisits);
-if (numVisits >= 1) {
-  visitCount.textContent = `Number of visits to our website: ${numVisits}. Thank you for stopping by!`;
+if (btn01) {
+  btn01.addEventListener("click", () => {
+    createModal1();
+  });
 }
+
+const closeModal = document.querySelector("#closeModal");
+if (closeModal) {
+  closeModal.addEventListener("click", () => {
+    modal.close();
+  });
+}
+if (document.getElementById("formResults")) {
+  displayFormData();
+}
+
+const visitCount = document.querySelector(".myVisitCount");
+let value = localStorage.getItem("numVisits");
+if (value) {
+  value = parseInt(value);
+  visitCount.textContent = `Number of visits to our website: ${value}. Thank you for stopping by!`;
+  value = value++;
+}
+localStorage.setItem("numVisits", value);
+
+
+// let numVisits = Number(window.localStorage.getItem("myVisitCount")) || 0;
+// console.log(numVisits);
+// if (numVisits >= 1) {
+//   visitCount.textContent = `Number of visits to our website: ${numVisits}. Thank you for stopping by!`;
+// }
+
+// const currentTime = Date.now();
+// if (value) {
+//   value = parseInt(value);
+//   const msTime = currentTime - value;
+//   const days = msTime / 86400000;
+//   if (days < 1) {
+//     visitCount.textContent = `Back so soon! Awesome!`;
+//   } else if (1 < days < 2) {
+//     visitCount.textContent = `You last visited ${days} day ago.`;
+//   } else if (days > 2) {
+//     visitCount.textContent = `You last visited ${days} days ago.`;
+//   }
+// }
+
--- project/scripts/weather.mjs
+++ project/scripts/weather.mjs
@@ -3,11 +3,14 @@
  const lock = "d5e4729c978ba0e7a32930e85c5fefae";
  const url = `//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${lock}&units=imperial`;
  const weatherContainer = document.querySelector("#weatherContainer");
-
+ const tenerifeLat = "28.46309623522285";
+ const tenerifeLong =  "-16.250801476640547";
+const tenerifeWeather = document.querySelector("#tenerifeWeather");
+const tenerifeUrl = `//api.openweathermap.org/data/2.5/weather?lat=${tenerifeLat}&lon=${tenerifeLong}&appid=${lock}&units=imperial`;
 
 
 function displayResults(data) {
-    console.log("json ready");
+    console.log("Las Palmas weather ready");
     let lasPalmas = document.createElement("h3");
     lasPalmas.textContent = `Las Palmas de Gran Canaria`;
     weatherContainer.appendChild(lasPalmas);
@@ -25,6 +28,25 @@ function displayResults(data) {
     weatherContainer.appendChild(feelsLike);
   }
   
+  function displayTenerifeResults(data) {
+    console.log("Tenerife weather ready");
+    let lasPalmas = document.createElement("h3");
+    lasPalmas.textContent = `Santa Cruz de Tenerife`;
+    tenerifeWeather.appendChild(lasPalmas);
+    let temp = document.createElement("p");
+    temp.textContent = `${data.main.temp}\u00b0 F`;
+    tenerifeWeather.appendChild(temp);
+    let descP = document.createElement("p");
+    descP.textContent = `${data.weather[0].description}`;
+    tenerifeWeather.appendChild(descP);
+    let humidity = document.createElement("p");
+    humidity.textContent = `humidity: ${data.main.humidity}`;
+    tenerifeWeather.appendChild(humidity);
+    let feelsLike = document.createElement("p");
+    feelsLike.textContent = `Feels like: ${data.main.feels_like}\u00b0 F`;
+    tenerifeWeather.appendChild(feelsLike);
+  }
+
 
 export async function weatherFetch() {
     try {
@@ -35,8 +57,18 @@ export async function weatherFetch() {
       const data = await response.json();
       console.log(data);
       displayResults(data);
-    } catch (error) {
+          } catch (error) {
       console.error(error.message);
     }
+    try {
+      const response = await fetch(tenerifeUrl);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data);
+      displayTenerifeResults(data);
+          } catch (error) {
+      console.error(error.message);
   }
-  
+}
